31be895850d568669cf6debe06ae23eeaa0f6a4b
Gustavo Onix <gustavolimaeduardo@gmail.com>
2021-09-21 18:16:57 -0300

    Ajustes no gréfico do index

---
+++ helpers/utils.js
@@ -0,0 +1,109 @@
+
+//***************************Gráfico de ofensores circular para as telas de relátorio**********************
+// var myChart = new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         labels: ['Abordagem', 'Fechamento', 'Ausência', 'Empatia', 'Segurança', 'Giria', 'Objetivo','Conhecimento','Sondagem','Argumento','Negociacao','Ética','Falta Cordialidade','Risco'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [of1,of2,of3,of4,of5,of6,of7,of8,of9,of10,of11,of12,of13,of14],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132)',
+//                 'rgba(54, 162, 235)',
+//                 'rgba(255, 206, 86)',
+//                 'rgba(75, 192, 192)',
+//                 'rgba(153, 102, 255)',
+//                 'rgba(255, 159, 64)',
+//                 '#0205d8',
+//                 '#d96848',
+//                 '#027518',
+//                 '#9275d8',
+//                 '#022511',
+//                 '#0266d8',
+//                 '#5250d8',
+//                 '#1436d8'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//       plugins: {
+//           title: {
+//             display: true,
+//             text: 'Média Geral por ofensores em %',
+//           }
+//       },
+//     }
+// });
+
+
+//***************************Grefico de linha para histórico de médias***********************************
+
+    // var ctx2 = document.getElementById('line');
+    // var myChart = new Chart(ctx2, {
+    //     type: 'line',
+    //     data: {
+    //         labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    //         datasets: [{
+    //             label: 'Felipe',
+    //             data: [],
+    //             backgroundColor: [
+    //                 '#d9534f',
+    //             ],
+    //             borderColor: [
+    //                 '#d9534f',
+    //             ],
+    //             borderWidth: 1
+    //           },
+    //           {
+    //             label: 'Camila',
+    //             data: [],
+    //             backgroundColor: [
+    //                 '#0275d8',
+    //             ],
+    //             borderColor: [
+    //                 '#0275d8',
+    //             ],
+    //             borderWidth: 1
+    //           },
+    //           {
+    //             label: 'Gustavo',
+    //             data: [],
+    //             backgroundColor: [
+    //                 '#292b2c',
+    //             ],
+    //             borderColor: [
+    //                 '#292b2c',
+    //             ],
+    //             borderWidth: 1
+    //           },
+    //           {
+    //             label: 'Geral',
+    //             data: [],
+    //             backgroundColor: [
+    //                 '#e2a24e',
+    //             ],
+    //             borderColor: [
+    //                 '#e2a24e',
+    //             ],
+    //             borderWidth: 1
+    //           }
+            
+    //         ],
+          
+    //   },        
+    //     options: {
+    //       responsive: true,
+    //       plugins: {
+    //         legend: {
+    //           position: 'top',
+    //         },
+    //         title: {
+    //           display: true,
+    //           text: 'Hístórico de Médias (*Em construção*)'
+    //         }
+    //       }
+          
+    //     }
+    // });
+    
