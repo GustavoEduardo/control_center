dab4c93dcc4c65044570662a9dcb076a6f03362e
Gustavo <40216172+GustavoEduardo@users.noreply.github.com>
2021-09-30 23:06:40 -0300

    pass aws

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('onix','root','135790',{
+const connection = new Sequelize('onix','root','13579Zero@',{
 	host: 'localhost',
 	dialect: 'mysql',
 	timezone: "-03:00"
