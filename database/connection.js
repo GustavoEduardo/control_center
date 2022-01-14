db52df884cd61f5f53dee9c37396a87ddec888ab
Gustavo <40216172+GustavoEduardo@users.noreply.github.com>
2022-01-14 20:36:11 -0300

    Delete database directory

--- database/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('onix','root','13579Zero@',{
-	host: 'localhost',
-	dialect: 'mysql',
-	timezone: "-03:00"
-});
-
-module.exports = connection;
